14634369eedabbdc8b198c58dd44e6a1a76f5170
DeepWalter <deepwalter.cn@gmail.com>
2019-04-13 23:27:40 +0800

    Add docs.

--- algorithms/src/main/java/algorithms/Queue.java
+++ algorithms/src/main/java/algorithms/Queue.java
@@ -1,6 +1,13 @@
 package algorithms;
 
-
+/**
+ * A {@code Queue} is a <em>first-in-first-out</em> collection. When iterating
+ * through the {@code Queue}, the order of elements is kept.
+ *
+ * @param <E> the type of elements in the {@code Queue}
+ *
+ * @author DeepWalter
+ */
 public interface Queue<E> extends Iterable<E>
 {
     void enqueue(E elem);
--- algorithms/src/main/java/algorithms/Stack.java
+++ algorithms/src/main/java/algorithms/Stack.java
@@ -1,8 +1,12 @@
 package algorithms;
 
 /**
+ * A (pushdown) {@code Stack} is a <em>last-in-first-out</em> collection. When
+ * iterating through the {@code Stack}, the order of elements is reversed.
  *
- * @param <E>
+ * @param <E> the type of elements in the {@code Stack}
+ *
+ * @author DeepWalter
  */
 public interface Stack<E> extends Iterable<E>
 {
