1d930fe268371b59e9ec5152f3b6f89a3e6ac414
DeepWalter <deepwalter.cn@gmail.com>
2019-04-14 20:20:07 +0800

    Add LinkedListQueue.

---
+++ algorithms/src/main/java/algorithms/LinkedListQueue.java
@@ -0,0 +1,77 @@
+package algorithms;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @param <E>
+ */
+public class LinkedListQueue<E> implements Queue<E>
+{
+    private Node head;
+    private Node tail;
+    private int N;
+
+    private class Node
+    {
+        E elem;
+        Node next;
+    }
+
+    @Override
+    public boolean isEmpty() { return N == 0; }
+
+    @Override
+    public int size() { return N; }
+
+    @Override
+    public E dequeue() throws NoSuchElementException
+    {
+        if(isEmpty()) throw new NoSuchElementException();
+
+        E elem = head.elem;
+        head = head.next;
+        N--;
+        if (isEmpty()) tail = null;
+
+        return elem;
+    }
+
+    @Override
+    public void enqueue(E elem)
+    {
+        Node newTail = new Node();
+        newTail.elem = elem;
+
+        if (isEmpty()) {
+            head = newTail;
+            tail = newTail;
+        } else {
+            tail.next = newTail;
+            tail = newTail;
+        }
+        N++;
+    }
+
+    @Override
+    public Iterator<E> iterator()
+    {
+        return new Iterator<E> ()
+        {
+            private Node iter = head;
+
+            public boolean hasNext() { return iter != null; }
+
+            public E next() throws NoSuchElementException
+            {
+                if (!hasNext()) throw new NoSuchElementException();
+
+                E elem = iter.elem;
+                iter = iter.next;
+
+                return elem;
+            }
+        };
+    }
+}
