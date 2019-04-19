c9a0b6639b84011c0dff79cbd9d34e58cc734e12
DeepWalter <deepwalter.cn@gmail.com>
2019-04-19 19:41:59 +0800

    Update docs.

--- algorithms/src/main/java/algorithms/FixedCapacityStack.java
+++ algorithms/src/main/java/algorithms/FixedCapacityStack.java
@@ -3,20 +3,36 @@ package algorithms;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+
+/**
+ * The {@code FixedCapacityStack} class is an implementation of the
+ * {@link Stack} interface using an (fixed size) array.
+ *
+ * @param <E> the type of elements in the {@code FixedCapacityStack}
+ */
 public class FixedCapacityStack<E> implements Stack<E>
 {
-    private E[] a ;  // stack entries
+    private E[] a ;     // stack entries
     private int N = 0;  // size
 
     @SuppressWarnings("unchecked")
-    public FixedCapacityStack(int capacity)
+    public FixedCapacityStack(int capacity) throws IllegalArgumentException
     {
+        if (capacity <= 0) throw new IllegalArgumentException(
+            "The capacity of the stack must be positive");
         a = (E[]) new Object[capacity];
     }
 
     @Override
     public boolean isEmpty() { return N == 0; }
 
+    /**
+     * Test whether this {@code FixedCapacityStack} has reached its maximal
+     * capacity.
+     *
+     * @return {@code true} if this {@code FixedCapacityStack} reached its
+     * maximal capacity; {@code false} otherwise
+     */
     public boolean isFull() { return N == a.length; }
 
     @Override
