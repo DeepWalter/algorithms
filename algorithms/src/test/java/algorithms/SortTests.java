6b8b6af18c652ea01674dadcf4b5104957b5c53b
DeepWalter <deepwalter.cn@gmail.com>
2019-04-21 00:36:41 +0800

    Adjust to parameterized tests.

--- algorithms/src/test/java/algorithms/SortTests.java
+++ algorithms/src/test/java/algorithms/SortTests.java
@@ -3,14 +3,32 @@ package algorithms;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class SortTests
 {
     private double[] arr;
     private int size = 1000;
+    private String alg;
+
+    public SortTests(String name)
+    {
+        alg = name;
+    }
+
+    @Parameters(name = "Test {index}: {0}")
+    public static Iterable<? extends Object> data()
+    {
+        return Arrays.asList("selection", "insertion");
+    }
 
     @Before
     public void setUP()
@@ -24,21 +42,39 @@ public class SortTests
         arr = null;
     }
 
-    @Test
-    public void testSelection()
-    {
-        assertFalse(Selection.isSorted(arr));
-        Selection.sort(arr);
-        assertTrue(Selection.isSorted(arr));
-    }
+    // @Test
+    // public void testSelection()
+    // {
+    //     assertFalse(Selection.isSorted(arr));
+    //     Selection.sort(arr);
+    //     assertTrue(Selection.isSorted(arr));
+    // }
+
+    // @Test
+    // public void testInsertion()
+    // {
+    //     assertFalse(Insertion.isSorted(arr));
+    //     Selection.sort(arr);
+    //     assertTrue(Insertion.isSorted(arr));
+    // }
 
     @Test
-    public void testInsertion()
+    public void testSort()
     {
-        assertFalse(Insertion.isSorted(arr));
-        Selection.sort(arr);
-        assertTrue(Insertion.isSorted(arr));
+        assertFalse(Sorted.isSorted(arr));
+
+        switch (alg.toLowerCase()) {
+            case "selection":
+                Selection.sort(arr);
+                break;
+            case "insertion":
+                Insertion.sort(arr);
+                break;
+
+            default:
+                break;
+        }
+
+        assertTrue(Sorted.isSorted(arr));
     }
-
-
 }
