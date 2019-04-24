f587e208143efee3bd69ea4dd2023d38539c7a6d
DeepWalter <deepwalter.cn@gmail.com>
2019-04-25 00:33:01 +0800

    Update docs.

--- algorithms/src/main/java/algorithms/ST.java
+++ algorithms/src/main/java/algorithms/ST.java
@@ -2,9 +2,39 @@ package algorithms;
 
 
 /**
+ * An unordered <em>symbol table</em>. A symbol table is a data structure for key-value pairs that
+ * supports two operations:
+ * <ul>
+ *   <li>{@link #put}: <em>insert</em> a new pair into the table;</li>
+ *   <li>{@link #get}: <em>search for</em> the value associated with a given key.</li>
+ * </ul>
+ * Besides these two operations, this interface also provides several other useful operations such
+ * as:
+ * <ul>
+ *  <li>{@link #delete}: <em>delete</em> a key and its value;</li>
+ *  <li>{@link #contains}: test if a key is in this symbol table;</li>
+ *  <li>{@link #keys}: generate an iterable object over all the keys in this symbol table;</li>
+ *  <li>{@link #size}: <em>count</em> the number of key-value pairs in this symbol table;</li>
+ *  <li>{@link #isEmpty}: test if this symbol table is empty.</li>
+ * </ul>
+ *
+ * <p>
+ * For simplicity, any implementation of this interface should adopt the following conventions:
+ * <ul>
+ *   <li>No duplicate keys: only one value is associated with each key;</li>
+ *   <li>Inserting a key-value pair with existing key overrides the associated value;</li>
+ *   <li>Keys must not be {@code null};</li>
+ *   <li>No key can be associated with the value {@code null}.</li>
+ * </ul>
+ * For more information, see Section 3.1 of <em>Algorithms, 4th edition</em>.
+ * </p>
  *
  * @param <K> the type of keys
  * @param <V> the type of values
+ *
+ * @author DeepWalter
+ *
+ * @see OrderedST
  */
 public interface ST<K, V>
 {
@@ -12,7 +42,7 @@ public interface ST<K, V>
      * Puts key-value pair into this symbol table, or removes {@code key} if {@code val} is
      * {@code null}.
      *
-     * @param key the key to put
+     * @param key the key to insert
      * @param val the value corresponding to the key
      */
     void put(K key, V val);
