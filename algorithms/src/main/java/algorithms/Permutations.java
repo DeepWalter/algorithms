b528e420175ec9ace104ed70bc2f282687cdd4b4
DeepWalter <deepwalter.cn@gmail.com>
2019-05-03 23:44:06 +0800

    Add functions for double and generic types.

--- algorithms/src/main/java/algorithms/Permutations.java
+++ algorithms/src/main/java/algorithms/Permutations.java
@@ -1,37 +1,167 @@
 package algorithms;
 
+import java.util.Arrays;
 
 /**
  * Alogrithms for computing permutations.
  */
 public final class Permutations
 {
+    /**
+     * Tests if the current permutation has a successor in lexicographic order.
+     *
+     * <p>
+     * <strong>Note:</strong><br>
+     * {@code array} has no successor iff its elements are in decreasing order. That is, the current
+     * permutation is the last one in lexicographic order.
+     *
+     * @param array the current permutation
+     * @return {@code true} if the current permutation has a successor in lexicographic order;
+     * {@code false} otherwise
+     */
     public static boolean hasNext(int[] array)
     {
-        int j = array.length - 2;   // index of the second to last element
-        while (j >= 0 && array[j] >= array[j+1]) {
-            j--;
+        // array has a next permutation iff it is not in decreasing order.
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] < array[i + 1]) {
+                return true;
+            }
         }
 
-        return j >= 0;
+        return false;
     }
+
+    /**
+     * Tests if the current permutation has a successor in lexicographic order.
+     *
+     * <p>
+     * <strong>Note:</strong><br>
+     * {@code array} has no successor iff its elements are in decreasing order. That is, the current
+     * permutation is the last one in lexicographic order.
+     *
+     * @param array the current permutation
+     * @return {@code true} if the current permutation has a successor in lexicographic order;
+     * {@code false} otherwise
+     */
+    public static boolean hasNext(double[] array)
+    {
+        // array has a next permutation iff it is not in decreasing order.
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] < array[i + 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Tests if the current permutation has a successor in lexicographic order.
+     *
+     * <p>
+     * <strong>Note:</strong><br>
+     * {@code array} has no successor iff its elements are in decreasing order. That is, the current
+     * permutation is the last one in lexicographic order.
+     *
+     * @param <T> the type of elements in {@code array}
+     * @param array the current permutation
+     * @return {@code true} if the current permutation has a successor in lexicographic order;
+     * {@code false} otherwise
+     */
+    public static <T extends Comparable<? super T>> boolean hasNext(T[] array)
+    {
+        // array has a next permutation iff it is not in decreasing order.
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i].compareTo(array[i + 1]) < 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Computes next permutation in-place.
+     *
+     * @param array the current permutation
+     */
     public static void next(int[] array)
     {
-        /** Length of {@code array}. */
         int length = array.length;
 
+        // Find the largest j s.t. array[j] < array[j+1].
         int j = length - 2;   // index of the second to last element
         while (j >= 0 && array[j] >= array[j+1]) {
             j--;
         }
-        if (j < 0) {
+        if (j < 0) {    // no such j; or equivalently, array is in decreasing order
             return;
         }
-        // at this point array[j] < array[j+1]
+
+        // Find the first l from right side s.t. array[l] > array[j].
         int l = length - 1;
-        while (array[l] <= array[j]) { // find the first l from right side s.t. array[l] > array[j]
+        while (array[l] <= array[j]) {
             l--;
         }
+
+        // at this point l >= j+1
+        swap(array, j, l);
+        reverse(array, j+1, length - 1);
+    }
+
+    /**
+     * Computes next permutation in-place.
+     *
+     * @param array the current permutation
+     */
+    public static void next(double[] array)
+    {
+        int length = array.length;
+
+        // Find the largest j s.t. array[j] < array[j+1].
+        int j = length - 2;   // index of the second to last element
+        while (j >= 0 && array[j] >= array[j+1]) {
+            j--;
+        }
+        if (j < 0) {    // no such j; or equivalently, array is in decreasing order
+            return;
+        }
+
+        // Find the first l from right side s.t. array[l] > array[j].
+        int l = length - 1;
+        while (array[l] <= array[j]) {
+            l--;
+        }
+
+        // at this point l >= j+1
+        swap(array, j, l);
+        reverse(array, j+1, length - 1);
+    }
+
+    /**
+     * Computes next permutation in-place.
+     *
+     * @param array the current permutation
+     */
+    public static <T extends Comparable<? super T>> void next(T[] array)
+    {
+        int length = array.length;
+
+        // Find the largest j s.t. array[j] < array[j+1].
+        int j = length - 2;   // index of the second to last element
+        while (j >= 0 && array[j].compareTo(array[j+1]) >= 0) {
+            j--;
+        }
+        if (j < 0) {    // no such j; or equivalently, array is in decreasing order
+            return;
+        }
+
+        // Find the first l from right side s.t. array[l] > array[j].
+        int l = length - 1;
+        while (array[l].compareTo(array[j]) <= 0) {
+            l--;
+        }
+
         // at this point l >= j+1
         swap(array, j, l);
         reverse(array, j+1, length - 1);
@@ -44,12 +174,57 @@ public final class Permutations
         array[j] = temp;
     }
 
+    private static void swap(double[] array, int i, int j)
+    {
+        double temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static <T extends Comparable<? super T>> void swap(T[] array, int i, int j)
+    {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    // Reverse the elements between index i and j (both included).
     private static void reverse(int[] array, int i, int j)
     {
         while (i < j) {
-            swap(array, i, j);
-            i++;
-            j--;
+            swap(array, i++, j--);
+        }
+    }
+
+    // Reverse the elements between index i and j (both included).
+    private static void reverse(double[] array, int i, int j)
+    {
+        while (i < j) {
+            swap(array, i++, j--);
+        }
+    }
+
+    // Reverse the elements between index i and j (both included).
+    private static <T extends Comparable<? super T>> void reverse(T[] array, int i, int j)
+    {
+        while (i < j) {
+            swap(array, i++, j--);
+        }
+    }
+
+    // test
+    public static void main(String[] args)
+    {
+        int N = 4;
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = i+1;
+        }
+
+        System.out.println(Arrays.toString(array));
+        while (hasNext(array)) {
+            next(array);
+            System.out.println(Arrays.toString(array));
         }
     }
 }
