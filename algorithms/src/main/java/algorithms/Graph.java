759df29552722d0c4342560e2f78e9af973e1145
DeepWalter <deepwalter.cn@gmail.com>
2019-05-05 13:40:11 +0800

    Add Graph interface.

---
+++ algorithms/src/main/java/algorithms/Graph.java
@@ -0,0 +1,36 @@
+package algorithms;
+
+
+/**
+ * An undirected graph.
+ */
+public interface Graph
+{
+    /**
+     * Returns the number of vertices in this graph.
+     *
+     * @return the number of vertices in this graph
+     */
+    int countVertices();
+
+    /**
+     * Returns the number of edges in this graph.
+     *
+     * @return the number of edges in this graph
+     */
+    int countEdges();
+
+    /**
+     * Adds an edge with vertices {@code v} and {@code w} into this graph
+     * @param v vertex of the edge
+     * @param w vertex of the edge
+     */
+    void addEdge(int v, int w);
+
+    /**
+     * Returns an iterable object over all adjacent vertices to {@code v}.
+     * @param v the vertex to match
+     * @return an iterable object over all adjacent vertices to {@code v}
+     */
+    Iterable<Integer> adjacentVerticesOf(int v);
+}
