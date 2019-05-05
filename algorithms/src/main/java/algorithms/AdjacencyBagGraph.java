aef7e976a6421a7bc9613c4abf99b327a4b33098
DeepWalter <deepwalter.cn@gmail.com>
2019-05-05 15:26:51 +0800

    Add addEdge and vertexCheck methods.

--- algorithms/src/main/java/algorithms/AdjacencyBagGraph.java
+++ algorithms/src/main/java/algorithms/AdjacencyBagGraph.java
@@ -45,6 +45,26 @@ public class AdjacencyBagGraph implements Graph
         return edgeCount;
     }
 
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param v vertex of the edge
+     * @param w vertex of the edge
+     * @throws IllegalArgumentException unless {@code 0 <= v < countVertices()} and
+     * {@code 0 <= w < countVertices()}
+     */
+    @Override
+    public void addEdge(int v, int w) throws IllegalArgumentException
+    {
+        vertexCheck(v);
+        vertexCheck(w);
+
+        adjBags[v].add(w);
+        adjBags[w].add(v);
+        edgeCount++;
+    }
+
     /**
      * {@inheritDoc}
      *
@@ -54,12 +74,31 @@ public class AdjacencyBagGraph implements Graph
     @Override
     public Iterable<Integer> adjacentVerticesOf(int v) throws IllegalArgumentException
     {
-        if (v < 0 || v >= vertexCount) {
-            throw new IllegalArgumentException("Argument to adjacentVerticesOf() must be in [0, "
-                + vertexCount + "), but " + v + " is given!");
-        }
+        vertexCheck(v);
 
         return adjBags[v];
     }
 
+    /**
+     * Checks if the given vertex is in this graph. If not, throws an appropriate runtime exception.
+     * @param v the vertex to check
+     * @throws IllegalArgumentException unless {@code 0 <= v < countVertices()}
+     */
+    private void vertexCheck(int v) throws IllegalArgumentException
+    {
+        if (v < 0 || v >= vertexCount) {
+            throw new IllegalArgumentException(illegalVertexMsg(v));
+        }
+    }
+
+    /**
+     * Constructs an IllegalArgumentException detail message.
+     *
+     * @param v the vertex
+     * @return a detail message
+     */
+    private String illegalVertexMsg(int v)
+    {
+        return "Vertex: " + v + ", not in the range [0, " + vertexCount + ").";
+    }
 }
