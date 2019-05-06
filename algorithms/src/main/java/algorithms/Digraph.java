445b20a45b9502045801f157244d68aa0e66d4c5
DeepWalter <deepwalter.cn@gmail.com>
2019-05-06 10:33:54 +0800

    Add some abstract methods.

--- algorithms/src/main/java/algorithms/Digraph.java
+++ algorithms/src/main/java/algorithms/Digraph.java
@@ -6,5 +6,42 @@ package algorithms;
  */
 public interface Digraph
 {
+    /**
+     * Returns the number of vertices in this digraph.
+     *
+     * @return the number of vertices in this digraph
+     */
+    int countVertices();
 
+    /**
+     * Returns the number of edges in this digraph.
+     *
+     * @return the number of edges in this digraph
+     */
+    int countEdges();
+
+    /**
+     * Adds an edge {@code v -> w} into this digraph.
+     *
+     * @param v head of the edge
+     * @param w tail of the edge
+     */
+    void addEdge(int v, int w);
+
+    /**
+     * Returns an iterable object over all vertices connecting to {@code v} by edges pointing from
+     * {@code v}.
+     *
+     * @param v the vertex to match
+     * @return an iterable object over all vertices connecting to {@code v} by edges pointing from
+     * {@code v}
+     */
+    Iterable<Integer> adjacentVerticesOf(int v);
+
+    /**
+     * Reverses this digraph.
+     *
+     * @return a copy of this digraph with each edge's direction reversed
+     */
+    Digraph reverse();
 }
