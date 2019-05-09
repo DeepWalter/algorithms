1c9ed8a3dc0b42613f5c88f54a8e2c22783f5f69
DeepWalter <deepwalter.cn@gmail.com>
2019-05-10 02:04:46 +0800

    Add more methods.

--- algorithms/src/main/java/algorithms/StringST.java
+++ algorithms/src/main/java/algorithms/StringST.java
@@ -3,5 +3,28 @@ package algorithms;
 
 public interface StringST<V> extends ST<String, V>
 {
+    /**
+     * Returns the longest key that is a prefix of {@code s}.
+     *
+     * @param s the string to match
+     * @return the longest key that is a prefix of {@code s}
+     */
+    String longestPrefixOf(String s);
 
+    /**
+     * Returns an iterable object over all keys having {@code s} as a prefix.
+     *
+     * @param s the prefix
+     * @return an iterable object over all keys having {@code s} as a prefix
+     */
+    Iterable<String> keysWithPrefix(String s);
+
+    /**
+     * Returns an iterable object over all keys that match {@code s}. Here, {@code .} matches any
+     * character.
+     *
+     * @param s the matching pattern
+     * @return an iterable object over all keys that match {@code s}
+     */
+    Iterable<String> keysThatMatch(String s);
 }
