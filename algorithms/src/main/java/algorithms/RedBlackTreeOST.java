0f6f7afeb88438f7a4439ea45969ddba9aa48dd2
DeepWalter <deepwalter.cn@gmail.com>
2019-05-14 23:41:06 +0800

    Add rank and select.

--- algorithms/src/main/java/algorithms/RedBlackTreeOST.java
+++ algorithms/src/main/java/algorithms/RedBlackTreeOST.java
@@ -1,5 +1,7 @@
 package algorithms;
 
+import java.util.NoSuchElementException;
+
 
 public class RedBlackTreeOST<K extends Comparable<? super K>, V> implements OST<K, V>
 {
@@ -119,6 +121,7 @@ public class RedBlackTreeOST<K extends Comparable<? super K>, V> implements OST<
     public void put(K key, V value)
     {
         root = put(key, value, root);
+        root.color = Color.BLACK;
     }
 
     /**
@@ -181,4 +184,63 @@ public class RedBlackTreeOST<K extends Comparable<? super K>, V> implements OST<
         }
     }
 
+    // TODO: delete
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param key the key to match
+     */
+    @Override
+    public int rank(K key)
+    {
+        return rank(key, root);
+    }
+
+    /**
+     * Returns the number of keys less than {@code key} in the subtree rooted at {@code node}.
+     *
+     * @param key the key to match
+     * @param node the root of the subtree
+     * @return the number of keys less than {@code key} in the subtree rooted at {@code node}
+     */
+    private int rank(K key, Node node)
+    {
+        if (node == null) return 0;
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return rank(key, node.left);
+        } else if (cmp > 0) {
+            return size(node.left) + 1 + rank(key, node.right);
+        } else {
+            return size(node.left);
+        }
+    }
+
+    @Override
+    public K select(int i) { return select(i, root); }
+
+    /**
+     * Returns the key with rank {@code i} in the subtree rooted at {@code node}.
+     *
+     * @param i the rank
+     * @param node the root of the subtree
+     * @return the key with rank {@code i} in the subtree rooted at {@code node}
+     * @throws NoSuchElementException unless {@code 0 <= i < size(node)}
+     */
+    private K select(int i, Node node)
+    {
+        // if subtree is empty or i is out of range
+        if (i < 0 || i >= size(node)) throw new NoSuchElementException();
+
+        int s = size(node.left);
+        if (i == s) {
+            return node.key;
+        } else if (i < s) {
+            return select(i, node.left);
+        } else {
+            return select(i - s - 1, node.right);
+        }
+    }
 }
